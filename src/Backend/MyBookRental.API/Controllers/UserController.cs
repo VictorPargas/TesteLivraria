c068a75cf563b0f999b40264afc38d7d6374f915
VictorPargas <victorparga765@gmail.com>
2025-02-01 17:40:26 -0300

    Implementando Fluent Validator nas regras de Negocio

--- src/Backend/MyBookRental.API/Controllers/UserController.cs
+++ src/Backend/MyBookRental.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBookRental.Application.UseCase.User.Register;
 using MyBookRental.Communication.Requests;
 using MyBookRental.Communication.Responses;
 
@@ -12,7 +13,11 @@ namespace MyBookRental.API.Controllers
         [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
         public IActionResult Register(RequestRegisterUserJson request)
         {
-            return Created();
+            var useCase = new RegisterUserUseCase();
+
+            var result = useCase.Execute(request);
+
+            return Created(string.Empty, result);
         }
     }
 }
---
+++ src/Backend/MyBookRental.Application/UseCase/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MyBookRental.Communication.Requests;
+
+namespace MyBookRental.Application.UseCase.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(user => user.Name).NotEmpty();
+            RuleFor(user => user.Email).NotEmpty().EmailAddress();
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+            RuleFor(user => user.Phone).NotEmpty().Matches(@"^\+?[1-9]\d{1,14}$");
+            RuleFor(User => User.Profile).NotEmpty().Must(type => type == "Administrador" || type == "Usuário");
+        }
+    }
+}
