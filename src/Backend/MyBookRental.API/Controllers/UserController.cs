8c49ff45c9a658023dc4ea5ed8aad8655f7745e1
VictorPargas <victorparga765@gmail.com>
2025-02-01 17:06:14 -0300

    feat: adiciionando endpoitn para registar um usuario

---
+++ src/Backend/MyBookRental.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBookRental.Communication.Requests;
+using MyBookRental.Communication.Responses;
+
+namespace MyBookRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
